c1d59b3560b0f9a6f207847c7bbe28a60286476d
Sitabja <sitabjapal03@gmail.com>
2020-05-12 23:50:49 +0530

    Alpha build:3 Pre Hosting

--- server/server.js
+++ server/server.js
@@ -1,40 +1,14 @@
 express=require('express')
 path=require('path')
-geocode=require('./geocode')
-forecast=require('./forecast')
 const app=express()
 const port=process.env.PORT || 3030
 const publicDirPath=path.join(__dirname,'..','public')
 app.use(express.static(publicDirPath))
-let data={}
 app.get('*',(req,res)=>{
     res.sendFile(path.join(publicDirPath,'index.html'))
     
 })
 
-// app.get('*',(req,res)=>{
-//     //return res.sendFile(path.join(publicDirPath,'index.html'))
-//     //console.log(req.query);
-    
-//     if(!req.query.address){
-//         return res.send({error:'You must provide an address'})
-//     }
-//     geocode(req.query.address,(error,{lat,long,location}={})=>{
-//         //console.log(lat,long);
-//         console.log(req.query);
-//         if(error){
-//             return res.send({error})
-//         }
-//         forecast(lat,long,(error,forecastData)=>{
-//             console.log(forecastData.current);
-            
-//             if(error){
-//                 return res.send({error})
-//             }
-//         })
-//     })
-// })
-
 app.listen(port,()=>{
-    console.log("Listening on port 3030");
+    console.log("Listening on port"+port);
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
 const path=require('path')
-
-module.exports={
-    entry:'./src/app.js',//entry point
-    output:{
-        path:path.join(__dirname,'public'),//Location of op file
-        filename:'bundles.js'
-    },
-    mode: 'development',
-    module:{
-        rules:[{
-            use: {
-                loader: "babel-loader",
-                query: {
-                    presets: ["@babel/preset-env", "@babel/preset-react"],
-                }
-            },
-            //This tells webpack to use babel-loader
-            test:/\.js$/, //regular exp of file edning with js
-            exclude:/node_modules/
-        },{
-            test:/\.s?css$/,
-            use:['style-loader','css-loader','sass-loader']
-        }]
-    },
-    devtool:'cheap-module-eval-source-map',
-    devServer:{
-        contentBase:path.join(__dirname,'public'),
-        historyApiFallback:true
+module.exports=(env)=>{
+    console.log("env: "+env);
+    const isProduction=env==='production'
+    return {
+        entry:'./src/app.js',//entry point
+        output:{
+            path:path.join(__dirname,'public'),//Location of op file
+            filename:'bundle.js'
+        },
+        mode: 'development',
+        module:{
+            rules:[{
+                use: {
+                    loader: "babel-loader",
+                    query: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    }
+                },
+                //This tells webpack to use babel-loader
+                test:/\.js$/, //regular exp of file edning with js
+                exclude:/node_modules/
+            },{
+                test:/\.s?css$/,
+                use:['style-loader','css-loader','sass-loader']
+            }]
+        },
+        devtool:isProduction?'source-map' :'cheap-module-eval-source-map',
+        devServer:{
+            contentBase:path.join(__dirname,'public'),
+            historyApiFallback:true
+        }
     
     }
 
+    
 }
